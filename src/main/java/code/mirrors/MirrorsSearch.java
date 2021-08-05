deb9867204c0f4861b51bf2c905e53ad9d6ce3cf
zhuqi <zhuqi@youzan.com>
2021-08-05 14:15:28 +0800

    mis

--- src/main/java/code/mirrors/MirrorsSearch.java
+++ src/main/java/code/mirrors/MirrorsSearch.java
@@ -1,6 +1,9 @@
 package code.mirrors;
 
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class MirrorsSearch {
 
 
@@ -11,6 +14,42 @@ public class MirrorsSearch {
 
         TreeNode(int x) {
             val = x;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(String[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        return createTree(nums, 0);
+    }
+
+    public TreeNode createTree(String[] nums, int i) {
+        if (i == nums.length) {
+            return null;
+        }
+        if (nums[i].equals("#")) {
+            return null;
+        }
+        TreeNode treeNode = new TreeNode(Integer.parseInt(nums[i]));
+        treeNode.left = createTree(nums, i + 1);
+        treeNode.right = createTree(nums, i + 2);
+        return treeNode;
+    }
+
+
+    public void print(TreeNode treeNode) {
+        Queue<TreeNode> stack = new ArrayDeque<>();
+        stack.add(treeNode);
+        while (!stack.isEmpty()) {
+            TreeNode pop = stack.poll();
+            System.out.print(pop.val + " ");
+            if (pop.left != null) {
+                stack.add(pop.left);
+            }
+            if (pop.right != null) {
+                stack.add(pop.right);
+            }
         }
     }
 
@@ -37,5 +76,12 @@ public class MirrorsSearch {
             cur = cur.left;
         }
 
+    }
+
+    public static void main(String[] args) {
+        MirrorsSearch search = new MirrorsSearch();
+        String[] nums = new String[]{"1","2","3","4","5","6","7","#","#","8","#","#","#","#","#"};
+        TreeNode tree = search.sortedArrayToBST(nums);
+        System.out.println(tree);
     }
 }
